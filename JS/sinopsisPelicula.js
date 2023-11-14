aba36b0e317b1e7c9da7a122b64331bcd42de239
TZippilli <tzippilli@udesa.edu.ar>
2023-11-14 13:36:01 -0300

    agrego trailer y mas videos en series

--- JS/sinopsisPelicula.js
+++ JS/sinopsisPelicula.js
@@ -1,16 +1,16 @@
-let queryString = location.search; 
+let queryString = location.search;
 let StringToObject = new URLSearchParams(queryString);
-let id = StringToObject.get('id'); 
+let id = StringToObject.get('id');
 
-let urlPeli =`https://api.themoviedb.org/3/movie/${id}?api_key=f216cd46b728d209895b1387e51e9182&language=en-US`
+let urlPeli = `https://api.themoviedb.org/3/movie/${id}?api_key=f216cd46b728d209895b1387e51e9182&language=en-US`
 
 console.log(urlPeli);
 
 fetch(urlPeli)
-    .then(function(response){
+    .then(function (response) {
         return response.json();
     })
-    .then(function(data){
+    .then(function (data) {
         console.log(data);
 
         let bloque = document.querySelector('.movieDetailContainer')
@@ -26,17 +26,17 @@ fetch(urlPeli)
                                 <ul class="generos"></ul>
                                 <p>${data.overview}</p>
                                 <p>Calificacion: ${data.vote_average}</p>
-                            </div>` 
+                            </div>`
 
-                            let generos = document.querySelector('.generos');
-                            for (let i=0; i<data.genres.length; i++) {
-                                generos.innerHTML += `<a href='generos.html?id=${data.genres[i].name}'>
+        let generos = document.querySelector('.generos');
+        for (let i = 0; i < data.genres.length; i++) {
+            generos.innerHTML += `<a href='generos.html?id=${data.genres[i].name}'>
                                                         <ul>${data.genres[i].name}</ul>
                                                         </a>`
-                            }
-  
+        }
+
     })
-    .catch(function(error){
+    .catch(function (error) {
         console.log(error);
     })
 
@@ -44,30 +44,30 @@ fetch(urlPeli)
 
 
 
-
-    let urlTrailer = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=f216cd46b728d209895b1387e51e9182&language=en-US`
-    fetch(urlTrailer)
-    .then(function(response){
+// agrego trailer y mas videos (opcional)
+let urlTrailer = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=f216cd46b728d209895b1387e51e9182&language=en-US`
+fetch(urlTrailer)
+    .then(function (response) {
         return response.json();
     })
-    .then(function(data){
+    .then(function (data) {
         console.log(data);
         let urlYoutube = 'https://www.youtube.com/embed/'
         let Trailer = document.querySelector('.Trailer')
         let videos = document.querySelector('.otrosVideos')
-        for (let i = 0; i< data.results.length; i++){                    
-            if (data.results[i].type == 'Trailer'){
+        for (let i = 0; i < data.results.length; i++) {
+            if (data.results[i].type == 'Trailer') {
                 Trailer.innerHTML += `<iframe src = "${urlYoutube + data.results[i].key}" width= "850px" height = "520px"></iframe>`
-                
+
                 break
             }
         }
-        for (let i = 0; i< data.results.length; i++){                    
-            if (data.results[i].type == 'Trailer'){
+        for (let i = 0; i < data.results.length; i++) {
+            if (data.results[i].type == 'Trailer') {
                 videos.innerHTML += `<iframe src = "${urlYoutube + data.results[i].key}" ></iframe>`
             }
         }
     })
-    .catch(function(error){
+    .catch(function (error) {
         console.log(error);
     })
--- JS/sinopsisSerie.js
+++ JS/sinopsisSerie.js
@@ -40,3 +40,36 @@ fetch(urlSerie)
     .catch(function(error){
         console.log(error);
     })
+
+
+// agrego trailer y mas videos (opcional)
+let urlTrailer = `https://api.themoviedb.org/3/tv/${id}/videos?api_key=f216cd46b728d209895b1387e51e9182&language=en-US`;
+
+fetch(urlTrailer)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        let urlYoutube = 'https://www.youtube.com/embed/';
+        let Trailer = document.querySelector('.Trailer');
+        let videos = document.querySelector('.otrosVideos');
+        
+        // busco el trailer y lo muestro en el div .Trailer
+        for (let i = 0; i < data.results.length; i++) {
+            if (data.results[i].type == 'Trailer') {
+                Trailer.innerHTML += `<iframe src="${urlYoutube + data.results[i].key}" width="850px" height="520px"></iframe>`;
+                break;
+            }
+        }
+
+        // muestro los otros videos en .otrosVideos
+        for (let i = 0; i < data.results.length; i++) {
+            if (data.results[i].type == 'Trailer') {
+                videos.innerHTML += `<iframe src="${urlYoutube + data.results[i].key}"></iframe>`;
+            }
+        }
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
