080d4f8a13f81ec69b297494df1a6a2c3d851fee
LPineda1 <lpineda@udesa.edu.ar>
2023-11-15 00:08:00 -0300

    arreglos generos

--- JS/resultados.js
+++ JS/resultados.js
@@ -1,28 +1,31 @@
-let consulta = location.search
-let stringToObject = new URLSearchParams(consulta)
-let buscamos = stringToObject.get('query')
+let consulta = location.search;
+let stringToObject = new URLSearchParams(consulta);
+let buscamos = stringToObject.get('query');
 
 let apiPeli = `https://api.themoviedb.org/3/search/movie?query=${buscamos}&api_key=f216cd46b728d209895b1387e51e9182&language=en-US&page=1&include_adult=false`;
+let infobusqueda = document.querySelector('.results');
 
 fetch(apiPeli)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
+    .then(response => response.json())
+    .then(data => {
         console.log(data);
-        let datos = data.results
         let container = document.querySelector('.resultsContainer');
+        if (data.results.length === 0) {
+            container.innerHTML = `No hay resultados para su búsqueda de películas: <strong>"${buscamos}"</strong>`;
+        } else {
+            container.innerHTML = `Estos son los títulos de películas que coinciden con tu búsqueda de <strong>"${buscamos}"</strong>:`;
             let peliculas = '';
-            for(let i=0; i<datos.length; i++){
+            data.results.forEach(movie => {
                 peliculas += `<article>
-                                    <a href='sinopsis.html?id=${datos[i].id}'>
-                                    <img src=${"https://image.tmdb.org/t/p/w300/" + datos[i].poster_path} alt='' />
-                                    <p>${datos[i].title}</p>  </a>
-                                </article>`
-            }
-            container.innerHTML = peliculas;   
+                                    <a href='sinopsisPelicula.html?id=${movie.id}'>
+                                        <img src=${"https://image.tmdb.org/t/p/w300/" + movie.poster_path} alt='' />
+                                        <p>${movie.title}</p>
+                                    </a>
+                                </article>`;
+            });
+            container.innerHTML += peliculas;
+        }
     })
-        
-    .catch(function(error){
-        console.log(error);
-    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
--- JS/sinopsisGenero.js
+++ JS/sinopsisGenero.js
@@ -8,5 +8,44 @@ let generoBuscadoDom = document.querySelector("#generoBuscado")
 generoBuscadoDom.innerText = genreName
 // // Obtener la lista de géneros
 let urlGeneros = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`
+let urlGeneroPeli = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&with_genres=${id}&with_watch_monetization_types=flatrate`;
+let peliculas = document.querySelector('.pelis_genero')
 
+fetch(urlGeneroPeli)
+.then(function (response) {
+    return response.json()
+}
+)
+.then(function (data) {
+    console.log(data);
+    for (let i = 0; i < data.results.length; i++) {
+        let agregar = false;
+        let listaId = data.results[i].genre_ids;
+        for (let index = 0; index < listaId.length; index++) {
+            if (listaId[index] == id) {
+                agregar = true
+            }
+            
+        }
+       if (agregar == true) {
+        let popular = data.results[i]
+        peliculas.innerHTML += ` <article class="cuadrado thor"> 
+                                <a class="aclickeo" href="./sinopsisPelicula.html?idPelicula=${popular.id}">
+                                <img src="https://image.tmdb.org/t/p/w500/${popular.poster_path}" alt="" class="img1">
+                                ${popular.title} (${popular.release_date})
+                                </a>
+                                </article>
+                                 `
+       }
+        
+      
 
+    }
+    return data;
+}
+)
+.catch(function (error) {
+    console.log(error);
+    return error;
+}
+)
